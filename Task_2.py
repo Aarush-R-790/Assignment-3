6626e1c3920fa78dc6a686c740d47f9eac20b368
Aarush-R-790 <aarush.rout.62131121@gmail.com>
2025-09-08 10:22:17 +0530

    Completed Task 2

---
+++ Task_2.py
@@ -0,0 +1,14 @@
+import math
+
+def sqrt(a):
+    return math.sqrt(a)
+def log(a):
+    return math.log(a)
+def sine(a):
+    return math.sin(a)
+    
+a = int(input("Enter a number : "))
+
+print("Square root :", sqrt(a))
+print("Logaritm :", log(a))
+print("Sine :", sine(a))
